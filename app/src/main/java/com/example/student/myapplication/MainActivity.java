e2767348904506888f1045f1a7ce09f47ad8f233
taishinu <taiket2304@gmail.com>
2019-09-07 16:06:10 +0700

    Update them phim thoat

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.myapplication;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     TextView tv1, tv2;
     EditText edttk, edtmk;
-    Button dangnhap;
+    Button dangnhap, thoat;
     CheckBox luu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         edttk = (EditText)findViewById(R.id.edtTK);
         edtmk = (EditText)findViewById(R.id.edtMK);
         luu = (CheckBox) findViewById(R.id.cbLuu);
+        thoat = (Button)findViewById(R.id.btnThoat);
         dangnhap = (Button)findViewById(R.id.btnDN);
         dangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(MainActivity.this,"Chào mừng bạn đăng nhập hệ thống, thông tin của bạn đã được lưu",Toast.LENGTH_SHORT);
                     toast.show();
                 }
+            }
+        });
+        thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder b=new AlertDialog.Builder(MainActivity.this);
+                a.setTitle("Thông Báo");
+                a.setMessage("Bạn có muốn thoát không?");
+                a.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                a.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
             }
         });
     }
